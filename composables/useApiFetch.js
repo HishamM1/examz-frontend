8e5b706ddf29d95a9f3ef65901ef481a7e578b6b
HishamM1 <hishammedhat0@gmail.com>
2023-10-05 08:43:00 +0300

    deploy

--- composables/useApiFetch.js
+++ composables/useApiFetch.js
@@ -7,19 +7,7 @@ export async function useApiFetch( path, options = {}) {
         headers['Authorization'] = `Bearer ${access_token.value}`
     }
 
-    // return await $fetch("https://examz.fly.dev" + path, {
-    //     credentials: "include",
-    //     watch: false,
-    //     ...options,
-    //     headers: {
-    //         "Accept": "application/json",
-    //         ...headers,
-    //         ...options?.headers
-    //     }
-    // })
-
-    
-    return await $fetch("http://127.0.0.1:8000" + path, {
+    return await $fetch("https://examz.fly.dev" + path, {
         credentials: "include",
         watch: false,
         ...options,
