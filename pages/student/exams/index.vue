e5acc01f2cad8d9c200d802e616d68f32dfe860a
HishamM1 <hishammedhat0@gmail.com>
2023-10-08 08:56:55 +0300

    fix empty exams

--- pages/student/exams/index.vue
+++ pages/student/exams/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="container mx-auto"  v-if="exams.length > 0">
+  <main class="container mx-auto" v-if="!loading">
     <div class="flex items-center flex-col gap-4 sm:flex-row sm:gap-0 justify-between my-6 dark:bg-gray-800">
       <div class="flex flex-col gap-2 sm:flex-row">
         <select v-model="filters.active" @change="filters.page = 1"
@@ -9,14 +9,13 @@
               d="m1 1 4 4 4-4" />
           </svg>
           <option value="" selected>All</option>
-          <option value="active" >Active</option>
+          <option value="active">Active</option>
           <option value="ended">Ended</option>
         </select>
 
         <select v-model="filters.sortBy" @change="filters.page = 1"
-        class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="m1 1 4 4 4-4" />
           </svg>
@@ -29,9 +28,8 @@
         </select>
 
         <select v-model="filters.status" @change="filters.page = 1"
-        class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-        >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="m1 1 4 4 4-4" />
           </svg>
@@ -52,7 +50,7 @@
         </div>
         <input type="text" v-model="filters.search" v-on:input="filters.search = $event.target.value"
           class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search for exams" @input="filters.page = 1"/>
+          placeholder="Search for exams" @input="filters.page = 1" />
       </div>
     </div>
 
@@ -61,20 +59,12 @@
       <div v-if="exams" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         <ExamCard v-for="exam in exams" :exam="exam" :key="exam.id" :role="'student'" />
       </div>
-      <div v-else>no exams</div>
-    </div>
-    <div class="container mx-auto" v-else>
-      <LoadingState />
     </div>
     <Pagination :links="links" @page-change="handlePageChange" />
   </main>
-  <div class="container mx-auto" v-else-if="loading">
+  <div class="container mx-auto" v-else>
     <LoadingState />
   </div>
-  <div v-else-if="!loading && exams.length == 0"
-            class="container mx-auto flex justify-center items-center h-[calc(100vh-100px)] sm:text-2xl md:text-4xl font-bold">
-            No Exams Yet, Try <NuxtLink to="/student/join" class="text-indigo-600 hover:text-indigo-500 underline mx-2">Joining a Teacher!</NuxtLink> </div>
-
 </template>
 <script setup>
 definePageMeta({
@@ -138,14 +128,14 @@ const getExams = async () => {
 }
 
 function handlePageChange(index, link) {
-    if(index == 0){
-      filters.page -= 1;
-    }
-    else if(index == links.value.length - 1){
-      filters.page += 1;
-    } else {
-      filters.page = +link.label
-    }
+  if (index == 0) {
+    filters.page -= 1;
+  }
+  else if (index == links.value.length - 1) {
+    filters.page += 1;
+  } else {
+    filters.page = +link.label
+  }
 }
 
 </script>
